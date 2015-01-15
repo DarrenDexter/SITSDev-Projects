747a5033cd3d9dff428c9b99a76512deac0988aa
DarrenDexter <Darren.Dexter@4PD0J12-LT-HU.tribalgroup.net>
2015-01-15 15:15:18 +0000

    Batch Test

--- test.bat
+++
@@ -1,8 +0,0 @@
-﻿
-S:
-cd S:\Documents\Product Development\Teams\TM2 - Geek Clique (Interfaces)\Documentation\GitHub\SitsDev
-"C:\Program Files (x86)\Git\bin\git" pull
-"C:\Program Files (x86)\Git\bin\git" add -A
-"C:\Program Files (x86)\Git\bin\git" commit -m "Batch Test"
-"C:\Program Files (x86)\Git\bin\git" push
-pause
